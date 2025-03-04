c9fc55fa1ca4b6f1a8c7f3c2066b01f53800df49
Lluís <lluis.revilla@gmail.com>
2025-03-04 23:13:27 +0100

    Rename object

--- R/aaa.R
+++ R/aaa.R
@@ -1,5 +1,5 @@
 pkg_state <- new.env(parent = emptyenv())
 
-package_fields <- c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances")
+PACKAGE_FIELDS <- c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances")
 
 BASE <- rownames(installed.packages(priority = "base"))
