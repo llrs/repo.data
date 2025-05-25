ba9494125dc2286febaef6e41f6430a45b6734bc
Lluís <lluis.revilla@gmail.com>
2025-05-25 17:14:02 +0200

    Add repo in options

--- R/aaa.R
+++ R/aaa.R
@@ -5,3 +5,5 @@ pkg_state <- new.env(parent = emptyenv())
 PACKAGE_FIELDS <- c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances")
 
 BASE <- tools::standard_package_names()$base
+
+options(repos = "https://CRAN.R-project.org")
