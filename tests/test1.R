7ed015f6699bc9dbe6f5ed2577c2861ab43d0fba
Lluís <lluis.revilla@gmail.com>
2025-05-25 11:18:03 +0200

    Add some tests

--- tests/test1.R
+++ tests/test1.R
@@ -1,7 +1,26 @@
 # Raw testing without using any package (testthat or tinytest) as described on WRE
 
 library("repo.data")
+# cran archive ####
 pkges <- c("BaseSet", "dplyr")
 cpk <- cran_archive(pkges)
 pkgs_out <- setdiff(pkges, cpk$package)
 length(pkgs_out) == 0
+cpk2 <- cran_archive(pkges)
+identical(cpk, cpk2)
+
+# cran alias ####
+pkges <- c("BaseSet", "dplyr")
+cpk <- cran_alias(pkges)
+pkgs_out <- setdiff(pkges, cpk$package)
+length(pkgs_out) == 0
+cpk2 <- cran_alias(pkges)
+identical(cpk, cpk2)
+
+# cran links ####
+pkges <- c("BaseSet", "dplyr")
+cpk <- cran_links(pkges)
+pkgs_out <- setdiff(pkges, cpk$package)
+length(pkgs_out) == 0
+cpk2 <- cran_links(pkges)
+identical(cpk, cpk2)
