711f8082dd3c979e5925c675799bc4c68d5df5b0
Lluís <lluis.revilla@gmail.com>
2025-06-08 16:48:13 +0200

    Set also some options from the beginning to see what happens

--- R/aaa.R
+++ R/aaa.R
@@ -6,4 +6,12 @@ PACKAGE_FIELDS <- c("Depends", "Imports", "LinkingTo", "Suggests", "Enhances")
 
 BASE <- tools::standard_package_names()$base
 
-options(repos = c("@CRAN@" = "https://CRAN.R-project.org"))
+.onAttach <- function(libname, pkgname) {
+    opts <- options(repos = c("@CRAN@" = "https://CRAN.R-project.org"),
+            available_packages_filters = c("CRAN", "duplicates"))
+    pkg_state$opts <- opts
+}
+
+.onDetach <- function(libpath) {
+    options(pkg_state$opts)
+}
