f3cd46f345972e3171bc540792c1c0483377df7a
Lluís <lluis.revilla@gmail.com>
2025-09-09 08:35:42 +0200

    Replace installed.package

--- R/cran_snapshot.R
+++ R/cran_snapshot.R
@@ -65,7 +65,8 @@ cran_snapshot <- function(date) {
 #' @rdname cran_date
 #' @importFrom utils installed.packages
 #' @examples
-#' ip <- installed.packages()
+#' # ip <- installed.packages()
+#' ip <- data.frame(Package = c("A3", "AER"), Version = c("1.0.0", "1.2-15"))
 #' cran_date(ip)
 cran_date <- function(versions) {
     if ((!is.data.frame(versions) || !is.matrix(versions)) && !all(c("Package", "Version") %in% colnames(versions))) {
