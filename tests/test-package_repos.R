a9e61479b346a8b30197b1f94470bea454cb96c8
Lluís <lluis.revilla@gmail.com>
2025-11-30 13:45:44 +0100

    Fix another issue on tests

--- tests/test-package_repos.R
+++ tests/test-package_repos.R
@@ -9,7 +9,7 @@ stopifnot("At least 6 columns" = cols >= 6L)
 # Expected colnames of first and last columns
 stopifnot(all.equal(colnames(pr)[1:2], c("Package", "Repository")))
 last_three_columns <- seq(from = cols-2, to = cols)
-stopifnot(all.equal(colnames(pr)[seq_len(cols)], c("Other", "Packages_deps", "Repos")))
+stopifnot(all.equal(colnames(pr)[last_three_columns], c("Other", "Packages_deps", "Repos")))
 stopifnot("No NA" = !anyNA(pr))
 
 # Test that Bioconductor packages get their dependencies too
