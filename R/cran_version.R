9200448da6df6db29f50c14dda350882f2b0ac85
Lluís <lluis.revilla@gmail.com>
2025-12-05 01:54:29 +0100

    Fix issues with cran_versions

--- R/cran_version.R
+++ R/cran_version.R
@@ -1,21 +1,22 @@
 #' Install a specific version of a package
-#' 
+#'
 #' Install a package from CRAN of a specific version.
-#' 
-#' Uses CRAN specific API <https://cran.r-project.org/package=%s&version=%s> to install a package. 
+#'
+#' Uses CRAN specific API <https://cran.r-project.org/package=%s&version=%s> to install a package.
 #' @param package Name of the package present on CRAN archive.
 #' @param version The version number.
-#' @param ... Other arguments passed to install.packages. 
+#' @param ... Other arguments passed to install.packages.
 #' @returns Same as `install.packages()`.
-#' @references CRAN pages. 
+#' @references CRAN pages.
 #' @importFrom utils install.packages
+#' @export
 #' @examples
 #' \dontrun{
-#' cran_version("testthat", "0.7.1")
+#' cran_version("repo.data", "0.1.5", lib = tempdir())
 #' }
 cran_version <- function(package, version, ...) {
   valid_package_name(package)
   version <- as.package_version(version)
   url_package <- sprintf("https://cran.r-project.org/package=%s&version=%s", package, as.character(version))
-  install.packages(url_package, ...)
+  install.packages(pkgs = url_package, repos = NULL, ...)
 }
