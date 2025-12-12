f4134e8dec6374541fc1f451c8a7ee3559f979f3
Lluís <lluis.revilla@gmail.com>
2025-12-12 01:53:32 +0100

    Add more tests

--- tests/test-cran_targets_links.R
+++ tests/test-cran_targets_links.R
@@ -1,6 +1,17 @@
 library("repo.data")
 
 # Test that it works
-ctl <- cran_targets_links()
+st1 <- system.time(ctl <- cran_targets_links())
 repo.data:::no_internet(ctl)
 stopifnot(colnames(ctl) == c("from_pkg", "from_Rd", "to_pkg", "to_target", "to_Rd", "n"))
+stopifnot("CRAN has documentation pages" = as.logical(NROW(ctl)))
+
+# Cache
+st2 <- system.time(ctl2 <- cran_targets_links())
+stopifnot(identical(ctl, ctl2))
+stopifnot(st2["elapsed"] < st["elapsed"])
+
+# Subsetting
+st3 <- system.time(ctl3 <- cran_targets_links("BaseSet"))
+stopifnot(identical(ctl, ctl3))
+stopifnot(st3["elapsed"] < st["elapsed"])
--- tests/test-help.R
+++ tests/test-help.R
@@ -3,14 +3,32 @@ library("repo.data")
 # Test that it works
 bhpnl <- base_help_pages_not_linked()
 repo.data:::no_internet(bhpnl)
+stopifnot("Column names not matching" = colnames(bhpnl) == c("Package", "Source"))
+stopifnot("No data on base_help_pages_not_linked" = as.logical(NROW(bhpnl)))
+
 bhpwl <- base_help_pages_wo_links()
 repo.data:::no_internet(bhpwl)
+stopifnot("No data on base_help_pages_wo_links" = as.logical(NROW(bhpwl)))
+stopifnot("Column names not matching" = colnames(bhpwl) == c("Package", "Source"))
+
 bhc <- base_help_cliques()
 repo.data:::no_internet(bhc)
+stopifnot("Column names not matching" = colnames(bhc) == c("from_pkg", "from_Rd", "clique", "to_pkg", "to_Rd", "n"))
+stopifnot("No data on base_help_cliques" = as.logical(NROW(bhc)))
+stopifnot("No links == 0L" = !anyNA(bhc$n))
 
+# CRAN
 chpnl <- cran_help_pages_not_linked()
 repo.data:::no_internet(chpnl)
+stopifnot("Column names not matching" = colnames(chpnl) == c("Package", "Source"))
+stopifnot("No data on cran_help_pages_not_linked" = as.logical(NROW(chpnl)))
+
 chpwl <- cran_help_pages_wo_links()
 repo.data:::no_internet(chpwl)
+stopifnot("Column names not matching" = colnames(chpwl) == c("Package", "Source"))
+stopifnot("No data on cran_help_pages_wo_links" = as.logical(NROW(chpwl)))
+
 chc <- cran_help_cliques()
 repo.data:::no_internet(chc)
+stopifnot("Column names not matching" = colnames(chc) == c("Package", "Source"))
+stopifnot("No data on cran_help_cliques" = as.logical(NROW(chc)))
