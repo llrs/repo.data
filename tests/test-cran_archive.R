7499cacd064c35d7e74e8a302b073f293c00595e
Lluís <lluis.revilla@gmail.com>
2026-01-04 23:24:49 +0100

    Checks on some packages and resetting the environment effect

--- tests/test-cran_archive.R
+++ tests/test-cran_archive.R
@@ -1,7 +1,21 @@
 library("repo.data")
 
 # Test that it works
+st <- system.time(ca <- cran_archive(packages = c("BaseSet", "experDesign")))
+repo.data:::no_internet(ca)
+stopifnot(colnames(ca) == c("Package", "Datetime", "Version", "User", "Size", "Status"))
+st1 <- system.time(ca2 <- cran_archive(packages = c("BaseSet", "experDesign")))
+stopifnot("Cache didn't worked" = st1[[3]] < st[[3]])
+stopifnot("Cache was not the same" = all.equal(ca, ca2))
+
+clean_cache()
+st2 <- system.time(ca3 <- cran_archive(packages = c("BaseSet", "experDesign")))
+repo.data:::no_internet(ca3)
+stopifnot("Clean cache restores initial" = st2[[3]] > st[[3]])
+stopifnot("Still same result" = all.equal(ca, ca3))
+
 ca <- cran_archive()
 repo.data:::no_internet(ca)
 stopifnot(colnames(ca) == c("Package", "Datetime", "Version", "User", "Size", "Status"))
-
+ca2 <- cran_archive()
+stopifnot("Cache returns the same for all packages" = all.equal(ca, ca2))
