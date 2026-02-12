0c7137ca5a487d94b1829e8fa9e033cb8b2cb8db
Lluís <lluis.revilla@gmail.com>
2026-02-13 00:36:05 +0100

    Release actions

--- R/cran_actions.R
+++ R/cran_actions.R
@@ -1,16 +1,41 @@
 ## What are the times between archival and unarchival?
 
+# Author: CRAN
+download_actions <- function() {
+    src <- file.path(
+        Sys.getenv("R_CRAN_PACKAGE_ACTIONS_URL",
+                   "rsync://CRAN.R-project.org/CRAN-actions"),
+        "actions.rds")
+    if (startsWith(src, "file://")) {
+        readRDS(substring(src, 8L))
+    } else {
+        dst <- tempfile()
+        system2("rsync", c(src, dst))
+        readRDS(dst)
+    }
+}
+
+
 #' Look at the CRAN actions db
 #'
 #' CRAN tracks movements of packages and the actions used (for example to report
 #' the number of manual actions taken by the volunteers).
+#'
+#' There are three possible actions with packages source code: publish, archive and remove.
+#' - Publish: Add it to CRAN's PACAKGES file, users can install that version.
+#' - Archive: Removed from CRAN's repository PACKAGES file so users can't access the package with [available.packages()].
+#'  Remains on CRAN archive: <https://cran.r-project.org/src/contrib/Archive/>.
+#' - Remove: Removed from CRAN's archive.
+#'
+#' @param silent A logical value to issue warnings about the data or not.
 #' @inheritParams base_alias
 #' @returns A data.frame with Date, Time, User, Action, Package and Version columns.
 #' `NA` if not able to collect the data from CRAN.
 #' @importFrom stats na.omit
-#' @importFrom utils head
+#' @export
 #' @examples
-#' # ca <- cran_actions()
+#' ca <- cran_actions(silent = TRUE)
+#' head(ca)
 cran_actions <- function(packages = NULL, silent = FALSE) {
     out <- cran_all_actions()
     if (is_not_data(out)) {
@@ -18,9 +43,8 @@ cran_actions <- function(packages = NULL, silent = FALSE) {
     }
     check_pkg_names(packages, NA)
     actions <- get_package_subset("full_cran_actions", packages)
-    # first_package <- !duplicated(actions$Package)
 
-    if (isTRUE(silent)) {
+    if (isFALSE(silent)) {
         warnings_actions(actions)
     }
     actions
@@ -31,19 +55,8 @@ cran_all_actions <- function() {
     if (!empty_env(env)) {
         return(pkg_state[[env]])
     }
-    if (check_r_version("4.6.0")) {
-        actions <- tools:::CRAN_package_actions()
-    } else {
-        src <- file.path(Sys.getenv("R_CRAN_PACKAGE_ACTIONS_URL",
-                                    "rsync://CRAN.R-project.org/CRAN-actions"), "actions.rds")
-        if (startsWith(src, "file://"))
-            actions <- readRDS(substring(src, 8L))
-        else {
-            dst <- tempfile()
-            system2("rsync", c(src, dst))
-            actions <- readRDS(dst)
-        }
-    }
+
+    actions <- download_actions()
 
     actions$Date <- charToDate(actions$Date, "%F")
     actions$User <- as.factor(actions$User)
@@ -51,9 +64,18 @@ cran_all_actions <- function() {
     if (!all(na.omit(actions$Action) %in% lev)) {
         warning("New action by CRAN: ", na.omit(setdiff(actions$Action, lev)), call. = FALSE)
     }
+
+
     actions$Action <- factor(actions$Action, levels = lev)
     actions$Package <- as.factor(actions$Package)
+
     actions <- sort_by(actions, ~Package + datetime2POSIXct(Date, Time) + Action)
+
+        # Fill those that don't have version is archived
+    missing_v <- which(is.na(actions[, "Version"]))
+    k <- actions$Action[missing_v - 1L] == "publish" & actions$Package[missing_v - 1L] == actions$Package[missing_v]
+    actions$Version[missing_v[k]] <- actions$Version[missing_v[k] - 1L]
+
     rownames(actions) <- NULL
     pkg_state[[env]] <- actions
     actions
@@ -68,6 +90,6 @@ warnings_actions <- function(actions) {
     dup <- duplicated(actions[, c("Package", "Version", "Action")])
     if (any(dup)) {
         warning("There are ", sum(dup), " packages with duplicated actions for the same version.\n",
-                "Explanation: This indicate a manual intervetion of the CRAN team.", call. = FALSE)
+                "Explanation: These indicate a manual intervention of the CRAN team.", call. = FALSE)
     }
 }
