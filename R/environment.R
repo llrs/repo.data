56a18c12e13314660df48e41141140aeacc2c3d8
Lluís <lluis.revilla@gmail.com>
2026-08-06 20:27:45 +0200

    Warning when data can't be downloaded

--- R/environment.R
+++ R/environment.R
@@ -5,7 +5,7 @@ empty_env <- function(name) {
 save_state <- function(name, out, verbose = TRUE) {
     # Use CRAN mirror if not set a default
     CRAN_baseurl()
-    
+
     if (empty_env(name)) {
         if (verbose) {
             name_msg <- if (!is.null(names(name))) names(name) else name
@@ -13,6 +13,7 @@ save_state <- function(name, out, verbose = TRUE) {
         }
         m <- tryCatch(out, warning = function(w) {NA}, error = function(e) {NA})
         if (is_not_data(m)) {
+            warning("Failed to download ", name_msg)
             return(NA)
         }
         pkg_state[[name]] <- m
@@ -23,16 +24,16 @@ save_state <- function(name, out, verbose = TRUE) {
 get_package_subset <- function(name, pkges) {
     stopifnot(is.character(name) && length(name) == 1L,
     "NULL or character vector" = is.null(pkges) || (is.character(pkges) && length(pkges)))
-    
+
     if (empty_env(name)) {
         return(NULL)
     }
-    
+
     df <- pkg_state[[name]]
-    
+
     if (is.null(pkges)) {
         return(df)
     }
-    
+
     df[pkg_in_x(df, pkges), , drop = FALSE]
 }
